1f7b6eef008c7ef79693a386733faa2afb607639
Bora Aydın <boraaydins@gmail.com>
2023-08-29 18:52:38 +0300

    Add files via upload

---
+++ shortly/js/script.js
@@ -0,0 +1,45 @@
+const input = document.getElementById('link-input');
+const linkForm = document.getElementById('link-form');
+const errMsg = document.getElementById('err-msg');
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+linkForm.addEventListener('submit', formSubmit);
+btn.addEventListener('click', navToggle);
+
+// Validate a URL
+function validURL(str) {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  );
+  return !!pattern.test(str);
+}
+
+function formSubmit(e) {
+  e.preventDefault();
+
+  if (input.value === '') {
+    errMsg.innerHTML = 'Boş bırakmayınız.';
+    input.classList.add('border-red');
+  } else if (!validURL(input.value)) {
+    errMsg.innerHTML = 'Geçerli bağlantı giriniz.';
+    input.classList.add('border-red');
+  } else {
+    errMsg.innerHTML = '';
+    input.classList.remove('border-red');
+    alert('Başarılı');
+  }
+}
+
+// Toogle Mobile Menu
+function navToggle(e) {
+  btn.classList.toggle('open');
+  menu.classList.toggle('flex');
+  menu.classList.toggle('hidden');
+}
